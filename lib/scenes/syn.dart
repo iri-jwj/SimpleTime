c55f4325ae82d41a9bef3c834950bc2695fb9af9
简伟杰 <iri.jwj@outlook.com>
2018-12-21 10:28:15 +0800

    ARoute完成

--- lib/scenes/syn.dart
+++ lib/scenes/syn.dart
@@ -31,9 +31,13 @@ class SynchronizeState extends State<SynchronizePage> {
           children: <Widget>[
             Center(
               child: IconButton(
+                color: Color.fromARGB(255, 0, 255, 255),
                 icon: Icon(Icons.add),
                 tooltip: 'to Add microsoft account',
                 onPressed: _jumpToNative,
+                highlightColor: Color.fromARGB(255, 0, 255, 255),
+                disabledColor:  Color.fromARGB(255, 0, 0, 255),
+                splashColor: Color.fromARGB(255, 0, 255, 0),
               ),
             ),
           ],
---
+++ lib/util/Routers.dart
@@ -0,0 +1,23 @@
+import 'package:annotation_route/route.dart';
+import 'package:flutter/material.dart';
+import 'package:project_flutter/util/Routers.internal.dart';
+
+@ARouteRoot()
+class ProjectRouter {
+  ARouterInternal internal = ARouterInternalImpl();
+
+  Widget getPage(ProjectRouterOption option) {
+    var result = internal.findPage(
+        ARouteOption(option.urlPattern, option.params), option);
+    if (result.state == ARouterResultState.FOUND) {
+      return result.widget;
+    } else {
+      return null;
+    }
+  }
+}
+
+class ProjectRouterOption {
+  String urlPattern;
+  Map<String, dynamic> params;
+}
